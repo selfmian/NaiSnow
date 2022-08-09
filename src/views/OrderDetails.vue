dea3afa0378881cf0bc914c4715b36d4b05013ab
薛皓腾 <11252787+xuehaoteng@user.noreply.gitee.com>
2022-08-09 21:18:22 +0800

    更新版本

--- src/views/OrderDetails.vue
+++ src/views/OrderDetails.vue
@@ -56,12 +56,15 @@ export default {
   justify-content: center;
   align-items: center;
   border-radius: 8px;
+  h3{
+    margin-bottom: 0;
+  }
   .btn-zai {
     height: 35px;
     border-color: #94999b;
     color: #94999b;
     border-radius: 5px;
-    margin-bottom: 30px;
+    margin-bottom: 25px;
   }
 }
 .x-cards {
@@ -80,6 +83,9 @@ export default {
     align-items: center;
     min-height: 50px;
   }
+}
+.info .van-card__content{
+  min-height: 30px;
 }
 .info .van-card__content > div {
   display: flex;
